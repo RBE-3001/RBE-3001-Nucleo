f0322e1cef166216ceaec6f359698b7591c07660
Hannah Baez <hrbaez@wpi.com>
2018-01-22 23:59:32 -0500

    Working Part 8 Lab 1

--- src/coms/LabServer.cpp
+++ src/coms/LabServer.cpp
@@ -94,7 +94,7 @@ void LabServer::event(float * packet){
 
   for(int i = 0; i < myPumberOfPidChannels; i++)
     {
-	  /*
+
 	  float position = myPidObjects[i]->GetPIDPosition();
       float velocity = myPidObjects[i]->getVelocity();
       float torque   = myPidObjects[i]->loadCell->read();
@@ -102,11 +102,11 @@ void LabServer::event(float * packet){
       packet[(i*3)+0] = position;
       packet[(i*3)+1] = velocity;
       packet[(i*3)+2] = torque;
-*/
+/*
 	  packet[(i*3)+0] = 150;
 	  packet[(i*3)+1] = 0.5;
 	  packet[(i*3)+2] = 7;
-
+*/
     }
 
 
